3aa21155b6c994b42cc4975f2575026156b6173c
Patricio López <lopezjuripatricio@gmail.com>
2015-12-31 12:21:56 -0300

    remove places routes

--- app/routes/api/v1/places.js
+++ app/routes/api/v1/places.js
@@ -5,33 +5,4 @@ const Place = mongoose.model('Place');
 
 const router = express.Router();
 
-router.route('/')
-  .get((req, res, next) => {
-    Place.find({}).lean()
-      .then(places => res.send(places))
-      .catch(next);
-  });
-
-router.route('/:campus')
-  .get((req, res, next) => {
-    const query = {
-      campus: req.params.campus,
-      identifier: req.params.campus,
-    };
-    Place.findPlace(query).lean()
-      .then(places => res.send(places))
-      .catch(next);
-  });
-
-router.route('/:campus/:identifier')
-  .get((req, res, next) => {
-    const query = {
-      campus: req.params.campus,
-      identifier: req.params.identifier,
-    };
-    Place.findPlaces(query).lean()
-      .then(places => res.send(places))
-      .catch(next);
-  });
-
 module.exports = router;
