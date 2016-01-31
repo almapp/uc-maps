92077ea730345473392785e38fcc6a6bd77c1371
Patricio López <lopezjuripatricio@gmail.com>
2016-01-31 09:40:46 -0300

    improve seed script

--- app/tasks/seed.js
+++ app/tasks/seed.js
@@ -9,9 +9,15 @@ const database = require('./../config/database');
 const models = require('../models');
 
 const Place = mongoose.model('Place');
+const url = 'https://almapp.github.io/uc-maps-seeds';
 
-const names = ['campuses', 'faculties', 'buildings', 'places']
-const urls = names.map(s => `https://almapp.github.io/uc-maps-seeds/${s}.json`);
+function files() {
+  return request({ uri: `${url}/index.json`, json: true })
+    .then(json => json.files.map(path => `${url}/${path}`))
+    .then(paths => paths.map(path => request({ uri: path, json: true })))
+    .then(Promise.all)
+    .then(jsons => jsons.reduce((array, json) => array.concat(json), []))
+}
 
 function hash(string) {
   return crypto.createHash('md5').update(string).digest('hex');
@@ -21,44 +27,42 @@ function reverse(coordinate) {
   return [coordinate[1], coordinate[0]];
 }
 
-Promise.all(urls.map(url => {
-  const options = {
-    uri: url,
-    json: true,
-  };
-  return request(options).then(jsons => {
-    return Place.create(jsons.map(json => {
-      json._id = json.identifier ? hash(json.identifier) : undefined;
-      json.parent = json.parent ? hash(json.parent.identifier) : null;
-      json.ancestors = (json.ancestors || []).map(j => hash(j.identifier));
+function exec() {
+  return Place.remove({})
+    .then(() => files())
+    .then(jsons => jsons.map(prepare))
+    .then(places => Place.create(places))
+    .then(places => console.log(`Created ${places.length} places.`))
+    .then(() => Place.ensureIndexes())
+}
 
-      if (!json.location) return json;
+function prepare(json) {
+  json._id = json.identifier ? hash(json.identifier) : undefined;
+  json.parent = json.parent ? hash(json.parent.identifier) : null;
+  json.ancestors = (json.ancestors || []).map(j => hash(j.identifier));
 
-      if (json.location.type === 'Point') {
-        if (json.location.coordinates[0] === 0 && json.location.coordinates[1] === 0) {
-          delete json.location;
-        } else {
-          json.location.coordinates = reverse(json.location.coordinates);
-        }
-      } else {
-        const rings = json.location.coordinates;
-        if (rings[0].length === 0) {
-          delete json.location;
-        } else {
-          json.location.coordinates = json.location.coordinates.map(ring => {
-            ring = ring.map(reverse);
-            if (ring[0]) ring.push(ring[0]);
-            return ring;
-          });
-        }
-      }
-      return json;
-    }));
-  });
-})).then(_ => {
-  return Place.ensureIndexes();
-}).then(_ => {
-  console.log('Seed complete');
-}).catch(err => {
-  console.error(err);
-})
+  if (!json.location) return json;
+
+  if (json.location.type === 'Point') {
+    if (json.location.coordinates[0] === 0 && json.location.coordinates[1] === 0) {
+      delete json.location;
+    } else {
+      json.location.coordinates = reverse(json.location.coordinates);
+    }
+  } else {
+    const rings = json.location.coordinates;
+    if (rings[0].length === 0) {
+      delete json.location;
+    } else {
+      json.location.coordinates = json.location.coordinates.map(ring => {
+        ring = ring.map(reverse);
+        if (ring[0]) ring.push(ring[0]);
+        return ring;
+      });
+    }
+  }
+  return json;
+}
+
+// Run this script
+exec().then(() => process.exit())
